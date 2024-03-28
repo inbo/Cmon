9e1e2e703dc437330ab4a3b85dd822d7c4d8a2d6
suzannalettens <84769506+suzannalettens@users.noreply.github.com>
2024-03-28 15:50:17 +0100

    Resolve conflict in script
    Deleted everything above and including ========
    Plus extra (useless) code added by git

--- src/functions/plotnr2plotid.R
+++ src/functions/plotnr2plotid.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #### Converting a PLOTNR to a hex-based PLOTID
 #### Bruno De Vos  - Nov 2023
 # PLOTNRs are unique 10x10 m CMON gridcel numbers for Flander Region 
@@ -6,13 +5,5 @@
 
 plotnr2plotid<-function(PLOTNR) {paste0("P",as.character(as.hexmode(PLOTNR)))}
 
-=======
-#### Converting a PLOTNR to a hex-based PLOTID
-#### Bruno De Vos  - Nov 2023
-# PLOTNRs are unique 10x10 m CMON gridcel numbers for Flander Region 
-# e.g. PLOTNR=98908211
 
-plotnr2plotid<-function(PLOTNR) {paste0("P",as.character(as.hexmode(PLOTNR)))}
-
->>>>>>> ace476ac6c9956ed2c1cfe3dadb7cd4153f9e61a
 # plotnr2plotid(PLOTNR)
